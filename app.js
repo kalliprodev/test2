eb0d21449dc8df246c2458e12daaaa81ba2b2672
kallipro <125512626+kallipro@users.noreply.github.com>
2024-08-09 12:32:58 +0500

    birshi test fayl

---
+++ app.js
@@ -0,0 +1,18 @@
+document.getElementById("new").innerHTML = 'qalaysizlar';
+
+
+document.getElementById('demo').innerHTML = 'oqiwshilar';
+
+document.getElementById('klass').innerHTML = 'id berilgen js code';
+
+
+
+document.getElementById("demo").style.fontSize = "25px";
+
+
+document.getElementById("demo").style.color = "red";
+
+
+document.getElementById("demo").style.backgroundColor = "yellow"; 
+
+
